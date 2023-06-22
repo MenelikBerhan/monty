1ed3402d5bc90f6c7bb369b3b3150ab7b1d7ba2f
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-22 04:46:59 +0300

    Add op command length check

--- utilities.c
+++ utilities.c
@@ -10,39 +10,39 @@ void assign_f(ins_t *ins)
 	char *op;
 
 	op = ins->opcode;
-	if (!op || op[0] == '#' || !strncmp(op, "nop", 3))
+	if (!op || op[0] == '#' || (!strncmp(op, "nop", 3) && strlen(op) == 3))
 		ins->f = nop;
-	else if (!strncmp(op, "push", 4))
+	else if (!strncmp(op, "push", 4) && strlen(op) == 4)
 		ins->f = push;
-	else if (!strncmp(op, "pop", 3))
+	else if (!strncmp(op, "pop", 3) && strlen(op) == 3)
 		ins->f = pop;
-	else if (!strncmp(op, "pall", 4))
+	else if (!strncmp(op, "pall", 4) && strlen(op) == 4)
 		ins->f = pall;
-	else if (!strncmp(op, "pint", 4))
+	else if (!strncmp(op, "pint", 4) && strlen(op) == 4)
 		ins->f = pint;
-	else if (!strncmp(op, "swap", 4))
+	else if (!strncmp(op, "swap", 4) && strlen(op) == 4)
 		ins->f = swap;
-	else if (!strncmp(op, "add", 3))
+	else if (!strncmp(op, "add", 3) && strlen(op) == 3)
 		ins->f = add;
-	else if (!strncmp(op, "sub", 3))
+	else if (!strncmp(op, "sub", 3) && strlen(op) == 3)
 		ins->f = sub;
-	else if (!strncmp(op, "div", 3))
+	else if (!strncmp(op, "div", 3) && strlen(op) == 3)
 		ins->f = _div;
-	else if (!strncmp(op, "mul", 3))
+	else if (!strncmp(op, "mul", 3) && strlen(op) == 3)
 		ins->f = mul;
-	else if (!strncmp(op, "mod", 3))
+	else if (!strncmp(op, "mod", 3) && strlen(op) == 3)
 		ins->f = mod;
-	else if (!strncmp(op, "pchar", 4))
+	else if (!strncmp(op, "pchar", 4) && strlen(op) == 4)
 		ins->f = pchar;
-	else if (!strncmp(op, "pstr", 4))
+	else if (!strncmp(op, "pstr", 4) && strlen(op) == 4)
 		ins->f = pstr;
-	else if (!strncmp(op, "rotl", 4))
+	else if (!strncmp(op, "rotl", 4) && strlen(op) == 4)
 		ins->f = rotl;
-	else if (!strncmp(op, "rotr", 4))
+	else if (!strncmp(op, "rotr", 4) && strlen(op) == 4)
 		ins->f = rotr;
-	else if (!strncmp(op, "stack", 5))
+	else if (!strncmp(op, "stack", 5) && strlen(op) == 5)
 		ins->f = _stack;
-	else if (!strncmp(op, "queue", 5))
+	else if (!strncmp(op, "queue", 5) && strlen(op) == 5)
 		ins->f = queue;
 	else
 		ins->f = NULL;
